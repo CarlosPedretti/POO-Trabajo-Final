ffe7a375c96902c12e9296d793349345a3fb24e3
CarlosPedretti <carlosluca150@gmail.com>
2023-05-28 20:51:31 -0300

    Movimiento Jugador
    -Actualización del movimiento del jugador, con respecto al código.
    
    -Cambios y ajustes en las animaciones de movimiento.

--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -7,7 +7,6 @@ public class MovementPlayer : MonoBehaviour
 
     public Rigidbody2D rb;
     public float moveSpeed;
-    public Vector2 forceToApply;
     public Vector2 PlayerInput;
     float horizontalMov;
     float verticalMov;
@@ -41,26 +40,10 @@ public class MovementPlayer : MonoBehaviour
     void FixedUpdate()
     {
         Vector2 moveForce = PlayerInput * moveSpeed;
-        moveForce += forceToApply;
-        forceToApply /= forceDamping;
-        if (Mathf.Abs(forceToApply.x) <= 0.01f && Mathf.Abs(forceToApply.y) <= 0.01f)
-        {
-            forceToApply = Vector2.zero;
-        }
+
         rb.velocity = moveForce;
 
-
     }
 
-
-
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        if (collision.collider.CompareTag("Bullet"))
-        {
-            forceToApply += new Vector2(-20, 0);
-            Destroy(collision.gameObject);
-        }
-    }
 
 }
